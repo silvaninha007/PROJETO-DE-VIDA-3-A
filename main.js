c85927c2cb04e3fb77d51c4357d1d5490631a2c2
SILVANA FRANCISCO ALVES <132940036+silvaninha007@users.noreply.github.com>
2025-06-17 13:50:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,11 +16,18 @@ for(let i=0;i < botoes.length; i++){
 
 const contadores = document.querySelectorAll (".contador");
 let tempoAtual = new Date ();
-const tempoObjetivo1 = new Date("2025-12-12T00:00:00") - tempoAtual;
+const tempoObjetivo1 = new Date("2025-12-12T00:00:00");
 contadores[0].textContent = tempoObjetivo1;
-const tempoObjetivo2 = new Date("2029-12-25T00:00:00") - tempoAtual;
+const tempoObjetivo2 = new Date("2029-12-25T00:00:00");
 contadores[1].textContent = tempoObjetivo2;
-const tempoObjetivo3 = new Date("2027-07-10T00:00:00") - tempoAtual;
+const tempoObjetivo3 = new Date("2027-07-10T00:00:00");
 contadores[2].textContent = tempoObjetivo3;
-const tempoObjetivo4 = new Date("2026-06-15T00:00:00") - tempoAtual;
+const tempoObjetivo4 = new Date("2026-06-15T00:00:00");
 contadores[3].textContent = tempoObjetivo4;
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  return segundos;
+}
