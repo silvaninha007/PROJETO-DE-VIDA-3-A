70226cb2eb7740f6c5aaf2180916526da2398ef8
SILVANA FRANCISCO ALVES <132940036+silvaninha007@users.noreply.github.com>
2025-06-24 13:47:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -29,5 +29,15 @@ function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
   let tempoFinal = tempoObjetivo - tempoAtual;
   let segundos = Math.floor(tempoFinal / 1000);
-  return segundos;
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  let anos = Math.floor(dias/ 365)
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  dias %= 365;
+
+  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
