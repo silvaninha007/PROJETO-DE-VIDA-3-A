8eed8eb87353b89f7f31aa4536121237e59b0186
SILVANA FRANCISCO ALVES <132940036+silvaninha007@users.noreply.github.com>
2025-08-05 16:27:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,29 +15,27 @@ for(let i=0;i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll (".contador");
-let tempoAtual = new Date ();
-const tempoObjetivo1 = new Date("2025-12-30T00:00:00");
-contadores[0]. textContent = calculaTempo (tempoObjetivo1);
-const tempoObjetivo2 = new Date("2026-12-31T00:00:00");
-contadores[1]. textContent = calculaTempo (tempoObjetivo2);
-const tempoObjetivo3 = new Date("2025-12-12T00:00:00");
-contadores[2]. textContent = calculaTempo (tempoObjetivo3);
-const tempoObjetivo4 = new Date("2030-12-31T00:00:00");
-contadores[3]. textContent = calculaTempo (tempoObjetivo4);
+const tempoObjetivo1 = new Date("2025-12-12T00:00:00");
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempoObjetivo2 = new Date("2028-12-31T00:00:00");
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+const tempoObjetivo3 = new Date("2025-12-31T00:00:00");
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+const tempoObjetivo4 = new Date("2026-12-31T00:00:00");
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
-function calculaTempo(tempoObjetivo) {
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-    let anos = Math.floor (dias / 365);
+function calculaTempo(tempoObjetivo){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor (tempoFinal/1000);
+let minutos = Math.floor (segundos/60);
+let horas = Math.floor (minutos/60);
+let dias = Math.floor (horas/24);
+let anos = Math.floor (dias/365);
 
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 60;
-    dias %= 365;
-    
-    return anos + "Anos" + dias + "Dias" + horas + "Horas"+ minutos + "Minutos" + segundos + "Segundos";
+segundos %= 60;
+minutos %=60;
+horas %=24;
+dias %=365;
+return anos + " Anos " + dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos ";
 }
